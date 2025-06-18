a1feeed592e1cbde972fef310b703237f2cc4f23
may-leth <mayleth9609@gmail.com>
2025-06-18 17:21:59 +0200

    refactor(ReviewController):Implement Dtos in getReview and addReview

--- src/main/java/com/FemCoders/MonsterShop/controllers/ReviewController.java
+++ src/main/java/com/FemCoders/MonsterShop/controllers/ReviewController.java
@@ -1,14 +1,14 @@
 package com.FemCoders.MonsterShop.controllers;
 
-import com.FemCoders.MonsterShop.dtos.Review.ReviewMapper;
 import com.FemCoders.MonsterShop.dtos.Review.ReviewRequest;
 import com.FemCoders.MonsterShop.dtos.Review.ReviewResponse;
-import com.FemCoders.MonsterShop.models.Review;
 import com.FemCoders.MonsterShop.services.ReviewService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/reviews")
@@ -21,15 +21,17 @@ public class ReviewController {
 
     @GetMapping("/{id}")
     public ResponseEntity<ReviewResponse> getReview(@PathVariable long id){
-        return reviewService.getReview(id)
-                .map(review -> ResponseEntity.ok(ReviewMapper.entityToDto(review)))
-                .orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+        try{
+            ReviewResponse response = reviewService.getReview(id);
+            return ResponseEntity.ok(response);
+        } catch (NoSuchElementException exception) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
     public ResponseEntity<ReviewResponse> addReview(@RequestBody @Valid ReviewRequest reviewRequest){
-        Review newReview = ReviewMapper.dtoToEntity(reviewRequest);
-        Review savedReview = reviewService.addReview(newReview);
-        return new ResponseEntity<>(ReviewMapper.entityToDto(savedReview), HttpStatus.CREATED);
+        ReviewResponse createdReview = reviewService.addReview(reviewRequest);
+        return new ResponseEntity<>(createdReview, HttpStatus.CREATED);
     }
 }
